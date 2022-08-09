94cd8a03ff802281118ace3d784ccf3858932188
Adam26114 <zakato26114@gmail.com>
2022-08-09 07:19:53 +0630

    9 Aug 2022

--- l2scrollprogress/app.js
+++ l2scrollprogress/app.js
@@ -1,7 +1,9 @@
 // console.log('hey');
 var getprogressbar = document.getElementById('progress-bar');
 
-window.onscroll = function(){scrollpoint()};
+window.onscroll = function(){
+    scrollpoint()
+};
 
 function scrollpoint(){
     console.log('i am working');
