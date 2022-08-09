72e95b6857e9235225ba7236b456b2bec333f435
Adam26114 <zakato26114@gmail.com>
2022-08-09 07:45:46 +0630

    9 Aug 2022

--- l2scrollprogress/app.js
+++ l2scrollprogress/app.js
@@ -8,8 +8,33 @@ window.onscroll = function(){
 function scrollpoint(){
     // console.log('i am working');
 
+
+        // scroll top
+        // project height
+        // current height
+
+        // project height - current height
+        // scroll top * 100 / (project height - current height);
+
+
     var getscrolltop = document.documentElement.scrollTop;
-    console.log(getscrolltop);
+    // console.log(getscrolltop);
+    var getscrollheight = document.documentElement.scrollHeight;
+    // console.log(getscrollheight);
+    var getclientheight = document.documentElement.clientHeight;
+    // console.log(getclientheight);
+
+    var calcheight = getscrollheight - getclientheight;
+    // console.log(calcheight);
+
+    // var getfinal = Math.round(( getscrolltop * 100 / calcheight ));
+    // console.log(getfinal);
+    
+    var getfinal = Math.round(( getscrolltop / calcheight ) * 100 );
+    console.log(getfinal);
+
+
+
 
 };
 
