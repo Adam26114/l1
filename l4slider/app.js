632c572bba6af14e126d2ed008ddd28463bb103a
Adam26114 <zakato26114@gmail.com>
2022-08-15 08:35:09 +0630

    15 Aug 2022

--- l4slider/app.js
+++ l4slider/app.js
@@ -1,1 +1,14 @@
-// console.log('hey');
+var slides = document.getElementsByClassName("carousel-item");
+// console.log(slides); HTML collection
+
+var dots = document.querySelectorAll(".dot")
+// console.log(dots); Nodelist
+
+
+var currslide = 1;
+
+carousel(currslide);
+
+function carousel(slidenum){
+    console.log(slidenum);
+}
