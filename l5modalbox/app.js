11419f45aaeb79ff68fc00af1e11e7baf89b546b
Adam26114 <zakato26114@gmail.com>
2022-08-16 08:00:16 +0630

    16 Aug 2022

--- l5modalbox/app.js
+++ l5modalbox/app.js
@@ -1,1 +1,21 @@
-// console.log('hey');
+var getmodal = document.getElementById('signupmodal');
+var getbtnsignup = document.getElementById("btn-signup");
+var getbtnclose = document.querySelector(".btn-close");
+var getbtnfullscreen = document.getElementById("btn-fullscreen");
+var getbtnclosescreen = document.getElementById("btn-closescreen");
+
+getbtnsignup.addEventListener("click",function(){
+    getmodal.style.display = "block";
+});
+
+getbtnclose.addEventListener("click",function(){
+    getmodal.style.display = "none";
+});
+
+window.onclick = function(e){
+    // console.log(e.target);
+
+    if(e.target === getmodal){
+        getmodal.style.display = "none";
+    }
+};
